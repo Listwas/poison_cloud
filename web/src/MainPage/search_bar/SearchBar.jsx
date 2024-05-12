f055ce40a0a80278d9059179de0625881169e33a
Listwas <listwa.patrykkr@gmail.com>
2024-05-12 20:30:27 +0200

    MainPage: added search bar (without button)

--- web/src/MainPage/search_bar/SearchBar.jsx
+++ web/src/MainPage/search_bar/SearchBar.jsx
@@ -1,10 +1,17 @@
 import styles from "./SearchBar.module.css";
 
 function SearchBar() {
+  var username = "user";
   return (
-    <div className={styles.main_container}>
-      <h2>currently poisoned: //username</h2>
-      <div className={styles.Search_bar}></div>
+    <div id={styles.centring}>
+      <div className={styles.main_container}>
+        <h2>
+          currently poisoned: <span className={styles.user}>{username}</span>
+        </h2>
+      </div>
+      <div className={styles.search_bar}>
+        <input type="text"></input>
+      </div>
     </div>
   );
 }
