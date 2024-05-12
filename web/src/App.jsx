dd2d04597023d93c981fe3083955f24ffea6df61
Listwas <listwa.patrykkr@gmail.com>
2024-05-12 17:43:17 +0200

    web: initial commit

---
+++ web/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+function App() {
+  return (
+    <div id="main_container">
+      <figure id="logo"></figure>
+      <nav id="left_nav"></nav>
+    </div>
+  );
+}
+
+export default App;
