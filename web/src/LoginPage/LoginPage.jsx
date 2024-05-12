ca066d7e141ca92fb8c99f73070ebf55396008c6
KlonekV5 <bartasv5@gmail.com>
2024-05-12 15:43:09 -0500

    web: register fixes

--- web/src/LoginPage/LoginPage.jsx
+++ web/src/LoginPage/LoginPage.jsx
@@ -22,7 +22,7 @@ function LoginPage() {
         <div className={styles.contLogin}>
             <form className={styles.logForm}>
                 <h2 className={styles.logH2}><b>Login</b></h2><br/>
-                <input type="text"  className={styles.logInput} id="login" placeholder="login"/><br/>
+                <input type="text" className={styles.logInput} id="login" placeholder="login"/><br/>
                 <input type="text" className={styles.logInput}  id="passwd" placeholder="passwd"/><br/>
                 <input type="button" className={styles.logInput} id="submit" value="Register" onClick={changeShowRegister}/>
                 <input type="button" className={styles.logInput} id="submit" value="Login" onClick={changeShowRegister}/>
--- web/src/RegisterPage/RegisterPage.jsx
+++ web/src/RegisterPage/RegisterPage.jsx
@@ -1,20 +1,37 @@
-import styles from"./RegisterPage.css";
+import styles from "./RegisterPage.module.css";
+import { useState, useRef } from "react";
 
 function RegisterPage() {
+    const [showLogin, setShowLogin] = useState(false);
+
+    if (showLogin) {
+        return (
+            <>
+                <LoginPage />
+            </>
+        );
+    }
+
+    const changeShowLogin= () => {
+        setShowLogin(true);
+    };
+
+
     return (
         <>
-        <div>
-            <form className={styles.contLogin}>
+        <div className={styles.contReg}>
+            <form className={styles.regForm}>
                 <h2 className={styles.logH2}><b>Register</b></h2><br/>
                 <input type="text" className={styles.logInput} id="name" placeholder="Name"/><br/>
                 <input type="text" className={styles.logInput} id="surname" placeholder="Surname"/><br/>
                 <input type="text" className={styles.logInput} id="login" placeholder="Login"/><br/>
                 <input type="text" className={styles.logInput} id="passwd" placeholder="Password"/><br/>
-                <input type="button" className={styles.logInput} id="submit" value="Register" OnClick=""/>
+                <input type="button" className={styles.logInput} id="submit" value="go to login" onClick={changeShowLogin}/>
+                <input type="button" className={styles.logInput} id="submit" value="Register" onClick=""/>
             </form>
         </div>
         </>
-    )
+    ) ;
 }
 
 export default RegisterPage;
