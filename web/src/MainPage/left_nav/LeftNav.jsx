363b18c861c2ee758b271d51077d9a9651983379
Listwas <listwa.patrykkr@gmail.com>
2024-05-12 21:01:51 +0200

    MainPage: added used space section to LeftNav

--- web/src/MainPage/left_nav/LeftNav.jsx
+++ web/src/MainPage/left_nav/LeftNav.jsx
@@ -2,13 +2,21 @@ import styles from "./LeftNav.module.css";
 
 function LeftNav() {
   return (
-    <div className={styles.left_nav}>
-      <nav>
-        <a href="url">my disk</a>
-        <a href="url">public files</a>
-        <a href="url">favourited</a>
-      </nav>
-    </div>
+    <>
+      <div className={styles.left_nav}>
+        <nav>
+          <a href="url">my disk</a>
+          <a href="url">public files</a>
+          <a href="url">favourited</a>
+        </nav>
+      </div>
+      <div className={styles.used_space}>
+        <h2>used space: </h2>
+        <hr></hr>
+        <br></br>
+        <a href="url">trash can</a>
+      </div>
+    </>
   );
 }
 export default LeftNav;
