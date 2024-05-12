28443029d5f7c5e767bd275e4f69e7a51bfcf702
Listwas <listwa.patrykkr@gmail.com>
2024-05-12 23:28:59 +0200

    MainPage: listing users from server response

--- web/src/MainPage/user_list/UserList.jsx
+++ web/src/MainPage/user_list/UserList.jsx
@@ -1,15 +1,32 @@
 import styles from "./UserList.module.css";
+import React, { useState, useEffect } from "react";
 
 function UserList() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://192.168.1.21:7708/active_users");
+        const data = await response.json();
+        console.log(data);
+        setUsers(data.users); // Update users state with the fetched data (an array of users)
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <>
       <div className={styles.user_list_container}>
         <h1>users: </h1>
         <div className={styles.list_style}>
           <ul>
-            <li>sad</li>
-            <li>sa</li>
-            <li>sad</li>
+            {users.map((user) => (
+              <li key={user}>{user}</li>
+            ))}
           </ul>
         </div>
       </div>
