139bd179284739487b56990d007fe7f6006a02d3
Listwas <listwa.patrykkr@gmail.com>
2024-05-13 17:09:21 +0200

    MainPage: search bar active user works

--- web/src/MainPage/search_bar/SearchBar.jsx
+++ web/src/MainPage/search_bar/SearchBar.jsx
@@ -1,12 +1,21 @@
 import styles from "./SearchBar.module.css";
+import { useEffect, useState } from "react";
+import { request } from "../../lib/server";
 
 function SearchBar() {
-  var username = "user";
+  const [userDetails, setUserDetails] = useState([]);
+
+  useEffect(() => {
+    request("GET", "/v1/user_details", (res) => {
+      setUserDetails(res.username);
+    });
+  }, []);
+
   return (
     <div id={styles.centring}>
       <div className={styles.main_container}>
         <h2>
-          currently poisoned: <span className={styles.user}>{username}</span>
+          currently poisoned: <span className={styles.user}>{userDetails}</span>
         </h2>
       </div>
       <div className={styles.search_bar}>
