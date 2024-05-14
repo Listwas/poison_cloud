44de0be4f131a6a60e0e75a6d177be0e0f6b5f44
Listwas <listwa.patrykkr@gmail.com>
2024-05-14 16:05:57 +0200

    web: don't show login page twice

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,16 +1,17 @@
 import "./App.css";
 import MainPage from "./MainPage/MainPage";
-import { useState} from "react";
+import { useState } from "react";
 import LoginPage from "./LoginPage/LoginPage";
+import { useCookies } from "react-cookie";
 
 function App() {
-
+  const [cookie, setCookie] = useCookies(["sessionKey"]);
   const [loginPage, setLoginPage] = useState(true);
 
-  if (loginPage) {
-    return(
+  if (loginPage && !("sessionKey" in cookie)) {
+    return (
       <>
-        <LoginPage setLoginPage={setLoginPage}/>
+        <LoginPage setLoginPage={setLoginPage} />
       </>
     );
   }
--- web/src/LoginPage/LoginPage.jsx
+++ web/src/LoginPage/LoginPage.jsx
@@ -1,57 +1,95 @@
 import styles from "./LoginPage.module.css";
-import RegisterPage from "../RegisterPage/RegisterPage"
+import RegisterPage from "../RegisterPage/RegisterPage";
 import { useState, useRef } from "react";
 import { requestPost } from "../lib/server";
-import { useCookies } from 'react-cookie';
+import { useCookies } from "react-cookie";
 
 function LoginPage({ setLoginPage }) {
-    const loginRef = useRef();
-    const passwdRef = useRef();
-    const [showRegister, setShowRegister] = useState(false);
-    const [cookies, setCookie] = useCookies(["sessionKey"]);
+  const loginRef = useRef();
+  const passwdRef = useRef();
+  const [showRegister, setShowRegister] = useState(false);
+  const [cookies, setCookie] = useCookies(["sessionKey"]);
 
-    if (showRegister) {
-        return (
-            <>
-                <RegisterPage returnToLogin={() => setShowRegister(false)} />
-            </>
-        );
-    }
-
-    function changeShowRegister() {
-        setShowRegister(true);
-    }
-
-    function loggedIn(response) {
-        console.log(response);
-        if (response.status == 200) {
-            alert("Pomyslnie zalogowano");
-            setCookie("sessionKey", response.session_key, {path: "/"});
-            setLoginPage(false);
-        }   else {
-            alert("Nie udalo sie zalogowac!");
-        }
-    }
-
-    function loginUser() {
-        const passwd = passwdRef.current.value;
-        const login = loginRef.current.value;
-        requestPost("/v1/user/login", {"username": login, "passwd": passwd}, loggedIn);
-    }
-
+  if (showRegister) {
     return (
-        <>
-        <div className={styles.contLogin}>
-            <form className={styles.logForm} onSubmit={(e) => {e.preventDefault()}}>
-                <h2 className={styles.logH2}><b>Login</b></h2><br/>
-                <input type="text" className={styles.logInput} id="login" ref={loginRef} placeholder="Login"/><br/>
-                <input type="password" className={styles.logInput}  id="passwd" ref={passwdRef} placeholder="Passwd"/><br/>
-                <input type="button" className={styles.logInput} id="reg" value="Register" onClick={changeShowRegister}/>
-                <input type="submit" className={styles.logInput} id="submit" value="Login" onClick={loginUser}/>
-            </form>
-        </div>
-        </>
+      <>
+        <RegisterPage returnToLogin={() => setShowRegister(false)} />
+      </>
     );
+  }
+
+  function changeShowRegister() {
+    setShowRegister(true);
+  }
+
+  function loggedIn(response) {
+    console.log(response);
+    if (response.status == 200) {
+      alert("Pomyslnie zalogowano");
+      setCookie("sessionKey", response.session_key, { path: "/" });
+      setLoginPage(false);
+    } else {
+      alert("Nie udalo sie zalogowac!");
+    }
+  }
+
+  function loginUser() {
+    const passwd = passwdRef.current.value;
+    const login = loginRef.current.value;
+    requestPost(
+      "/v1/user/login",
+      { username: login, passwd: passwd },
+      loggedIn
+    );
+  }
+
+  return (
+    <>
+      <div className={styles.contLogin}>
+        <form
+          className={styles.logForm}
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <h2 className={styles.logH2}>
+            <b>Login</b>
+          </h2>
+          <br />
+          <input
+            type="text"
+            className={styles.logInput}
+            id="login"
+            ref={loginRef}
+            placeholder="Login"
+          />
+          <br />
+          <input
+            type="password"
+            className={styles.logInput}
+            id="passwd"
+            ref={passwdRef}
+            placeholder="Passwd"
+          />
+          <br />
+          <input
+            type="button"
+            className={styles.logInput}
+            id="reg"
+            value="Register"
+            onClick={changeShowRegister}
+          />
+          <input
+            type="submit"
+            className={styles.logInput}
+            id="submit"
+            value="Login"
+            onClick={loginUser}
+          />
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default LoginPage;
